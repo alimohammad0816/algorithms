3dbd8700e0e1382aaeaef8c32696c42689a604a1
alimohammad0816 <alimohammad0816@gmail.com>
2021-09-30 19:02:35 +0330

    binary search

---
+++ binary_search.py
@@ -0,0 +1,21 @@
+"""
+    binary search
+    [2, 3, 4, 6, 12, 19, 20, 21], 12 ==> 5
+"""
+
+
+def binary_search(array, element):
+    low, high = 0, len(array) - 1
+
+    while low <= high:
+        mid = (high + low) // 2
+        val = array[mid]
+        if val == element:
+            return mid
+        elif val < element:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+
+print(binary_search([2, 3, 4, 6, 12, 19, 20, 21], 12))
